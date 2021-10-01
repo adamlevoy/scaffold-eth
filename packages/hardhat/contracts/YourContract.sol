5e6c5033aeefef8d7d04e1d04e9cdbaf90464d61
Adam Levoy <ajlevoy86@gmail.com>
2021-10-01 10:34:12 -0700

    added function to add amount to count, required sender to be owner

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -9,6 +9,14 @@ contract YourContract {
     // declare count
     uint256 public count;
 
+    // declare owner
+    address public owner;
+
+    constructor() {
+        // set owner to sender
+        owner = 0x99E628ef85CDD8fb1C088f8658008192bc85021a;
+    }
+
     // function to get the count, dupicative because count is public
     // function get() public view returns (uint256) {
     //     return count;
@@ -16,11 +24,20 @@ contract YourContract {
 
     // function to increment count by 1
     function inc() public {
+        // condition that requires
+        require(msg.sender == owner, "You are not the owner!");
         count++;
     }
 
     // function to decrement count
     function dec() public {
+        require(msg.sender == owner, "You are not the owner!");
         count--;
+    }
+
+    // function to increase count by amount
+    function add(uint256 amount) public {
+        require(msg.sender == owner, "You are not the owner!");
+        count += amount;
     }
 }
