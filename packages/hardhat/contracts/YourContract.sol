89b60439c073965e14b69bcf2e5835bce65a7846
Adam Levoy <ajlevoy86@gmail.com>
2021-11-12 12:57:47 -0800

    playing with arrays in solidity, remove and add item functions

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -6,52 +6,24 @@ import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
-    address public owner = 0x99E628ef85CDD8fb1C088f8658008192bc85021a;
+    string[] namesArray = ["adam", "rick", "morty"];
 
-    struct Todo {
-        string task;
-        bool completed;
-        // bools default value is false
+    function getName(uint256 index) public view returns (string memory) {
+        return namesArray[index];
     }
 
-    Todo[] public todos;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the owner!");
-        _;
+    function getAllNames() public view returns (string[] memory) {
+        return namesArray;
     }
 
-    function addTodo(string memory _task) public onlyOwner {
-        // 3 ways to initilize a struct
-        // like a function
-        todos.push(Todo(_task, false));
-
-        // key value pair
-        // todos.push(Todo({task: _task, completed: false}));
-
-        // init empty struct then update
-        // Todo memory todo;
-        // todo.task = _task;
-        // todos.push(todo);
+    function addName(string memory newName) public returns (string[] memory) {
+        namesArray.push(newName);
+        return namesArray;
     }
 
-    function updateTodo(uint256 _index, string memory _task) public onlyOwner {
-        Todo storage todo = todos[_index];
-        todo.task = _task;
-    }
-
-    function toggleCompleted(uint256 _index) public onlyOwner {
-        Todo storage todo = todos[_index];
-        todo.completed = !todo.completed;
-    }
-
-    // free getter function with public variable
-    function getTodo(uint256 _index)
-        public
-        view
-        returns (string memory text, bool completed)
-    {
-        Todo storage todo = todos[_index];
-        return (todo.task, todo.completed);
+    function removeName(uint256 index) public returns (string[] memory) {
+        namesArray[index] = namesArray[namesArray.length - 1];
+        namesArray.pop();
+        return namesArray;
     }
 }
