37627b032fea2eb23adbb4629960a9499b60af40
Adam Levoy <ajlevoy86@gmail.com>
2021-11-12 14:20:20 -0800

    mappings example with allowance contract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -5,25 +5,28 @@ import "hardhat/console.sol";
 
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
+// example nested map in allowance contract
 contract YourContract {
-    string[] namesArray = ["adam", "rick", "morty"];
+    // mapping address to another mapping
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    function getName(uint256 index) public view returns (string memory) {
-        return namesArray[index];
+    function setAllowance(
+        address _addrOwner,
+        address _addrSpender,
+        uint256 _amount
+    ) public {
+        allowance[_addrOwner][_addrSpender] = _amount;
     }
 
-    function getAllNames() public view returns (string[] memory) {
-        return namesArray;
+    function getAllowance(address _addrOwner, address _addrSpender)
+        public
+        view
+        returns (uint256)
+    {
+        return allowance[_addrOwner][_addrSpender];
     }
 
-    function addName(string memory newName) public returns (string[] memory) {
-        namesArray.push(newName);
-        return namesArray;
-    }
-
-    function removeName(uint256 index) public returns (string[] memory) {
-        namesArray[index] = namesArray[namesArray.length - 1];
-        namesArray.pop();
-        return namesArray;
+    function removeAllowance(address _addrOwner, address _addrSpender) public {
+        delete allowance[_addrOwner][_addrSpender];
     }
 }
